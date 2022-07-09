6802508e2bef6b4f1ee4636b30633b96e48034fe
Cesare De Cal <hi@cesare.io>
2022-07-09 21:19:03 +0200

    Format 01-variables.go

--- 01-variables.go
+++ 01-variables.go
@@ -4,60 +4,60 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello my friends!")
+	fmt.Println("Hello my friends!")
 
-    // Declaring and assigning the variable
-    var favoriteBook = "Harry Potter"
-    fmt.Println(favoriteBook)
+	// Declaring and assigning the variable
+	var favoriteBook = "Harry Potter"
+	fmt.Println(favoriteBook)
 
-    // Reassigning
-    favoriteBook = "Power of Habits"
-    fmt.Println(favoriteBook)
+	// Reassigning
+	favoriteBook = "Power of Habits"
+	fmt.Println(favoriteBook)
 
-		// Go is a strongly typed language
-		// This is not allowed:		
-    // favoriteBook = 12
+	// Go is a strongly typed language
+	// This is not allowed:		
+	// favoriteBook = 12
 
-		// Go is able to infer the type without us
-		// having to specify it. However, we can still
-		// explicitly defining the type
-		var otherFavoriteBook string = "Bad Blood"
-		fmt.Println(otherFavoriteBook)
+	// Go is able to infer the type without us
+	// having to specify it. However, we can still
+	// explicitly defining the type
+	var otherFavoriteBook string = "Bad Blood"
+	fmt.Println(otherFavoriteBook)
 
-		// You have to specify the type if you're
-		// not specifying a value to it
-		// If you don't assign it to anything 
-		// it is given the default value
-		var thirdFavoriteBook string
-		var myAge int
-		var amICool bool
+	// You have to specify the type if you're
+	// not specifying a value to it
+	// If you don't assign it to anything 
+	// it is given the default value
+	var thirdFavoriteBook string
+	var myAge int
+	var amICool bool
 
-		thirdFavoriteBook = "Diary of Wimpy Kid"
+	thirdFavoriteBook = "Diary of Wimpy Kid"
 
-		fmt.Println(thirdFavoriteBook, myAge, amICool)
+	fmt.Println(thirdFavoriteBook, myAge, amICool)
 
-		// Compound creation
-		// Creating multiple variables
-		// var favoriteNumber, favoriteChocolate, favoriteTeam = 213, "KitKat", "Knicks" 
-	
-		// Block creation
-		var (
-			favoriteNumber = 27
-			favoriteChocolate = "KitKat"
-			favoriteTeam = "Knicks"
-		)
+	// Compound creation
+	// Creating multiple variables
+	// var favoriteNumber, favoriteChocolate, favoriteTeam = 213, "KitKat", "Knicks" 
 
-		fmt.Println(favoriteNumber, favoriteChocolate, favoriteTeam)
+	// Block creation
+	var (
+		favoriteNumber = 27
+		favoriteChocolate = "KitKat"
+		favoriteTeam = "Knicks"
+	)
 
-		// Shortcut for declaring/assigning a variable:
-		favoriteAnimal := "Tiger"
-		fmt.Println(favoriteAnimal)
+	fmt.Println(favoriteNumber, favoriteChocolate, favoriteTeam)
 
-		pet1, pet2, pet3 := "cat", "dog", "rat"
-		fmt.Println(pet1, pet2, pet3)
+	// Shortcut for declaring/assigning a variable:
+	favoriteAnimal := "Tiger"
+	fmt.Println(favoriteAnimal)
 
-		// Constants
-		const myName = "Cesare"
-		// Not possible:
-		// myName = "Sarah"
-	}
+	pet1, pet2, pet3 := "cat", "dog", "rat"
+	fmt.Println(pet1, pet2, pet3)
+
+	// Constants
+	const myName = "Cesare"
+	// Not possible:
+	// myName = "Sarah"
+}
